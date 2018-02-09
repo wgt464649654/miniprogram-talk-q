b6a283d077337f8fa7d549b8eb33f5e10b0003a7
spark <464649654@qq.com>
2018-02-09 23:05:24 +0800

    add

--- 对话机器人/pages/index/index.js
+++ 对话机器人/pages/index/index.js
@@ -46,10 +46,12 @@ Page({
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    if (e.detail.errMsg.indexOf('fail') == -1) {
+      this.setData({
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
+      })
+    }
   },
 
   onShareAppMessage: function () {
--- 对话机器人/pages/main/game.js
+++ 对话机器人/pages/main/game.js
@@ -3,7 +3,8 @@ Page({
     userLeft: 5,
     addNum: 1.5,
     showStartButtom: true,
-    startButtomText: '开始'
+    startButtomText: '开始',
+    showLevel: true
   },
 
   run: function() {
@@ -11,18 +12,24 @@ Page({
       this.setData({
         userLeft: 90
       })
-      if ((new Date().getTime() - this.data.startTime)/1000 < 10) {
+      if ((new Date().getTime() - this.data.startTime) / 1000 < this.data.level) {
         this.setData({
-          result: 'win',
-          canRun: false,
-          showStartButtom: true,
-          startButtomText: '重新开始',
-          userLeft: 5,
-          addNum: 2,
-          robotClass: '',
-          victoryTime: new Date().getTime(),
+          result: 'win'
+        })
+      } else {
+        this.setData({
+          result: 'lose'
         })
       }
+      this.setData({
+        canRun: false,
+        showStartButtom: true,
+        startButtomText: '重新开始',
+        userLeft: 5,
+        addNum: 2,
+        robotClass: '',
+        victoryTime: new Date().getTime(),
+      })
       return;
     }
     this.setData({
@@ -31,16 +38,33 @@ Page({
   },
 
   start: function() {
+    if (!this.data.level) {
+      return;
+    }
     this.setData({
       canRun: true,
-      robotClass: 'robotr-run-10',
+      robotClass: 'robotr-run-' + this.data.level,
       showStartButtom: false,
       startTime: new Date().getTime()
     })
+    if (Math.floor(Math.random() * 10) > 5) {
+      setTimeout(() => {
+        this.setData({
+          reward: {
+            name: '闪现'
+          }
+        })
+      }, 5000)
+      setTimeout(() => {
+        this.setData({
+          reward: ''
+        })
+      }, 6000)
+    }
   },
 
   close: function() {
-    if ((new Date().getTime() - this.data.victoryTime)/1000 < 2) {
+    if ((new Date().getTime() - this.data.victoryTime)/1000 < 1.2) {
       return;
     }
     this.setData({
@@ -54,4 +78,27 @@ Page({
     })
   },
 
+  selectLevel: function(e) {
+    this.setData({
+      level: e.currentTarget.dataset.level,
+      levelText: e.currentTarget.dataset.text,
+      showLevel: false
+    })
+  },
+
+  reSelectLevel: function() {
+    this.setData({
+      showLevel: true
+    })
+  },
+
+  getReward: function(e) {
+    let name = e.currentTarget.dataset.reward;
+    if (name == '闪现') {
+      this.setData({
+        userLeft: this.data.userLeft + 10,
+        reward: ''
+      })
+    }
+  }
 })
